dc500d5e96e60d340952a1a5a4aaa01e5aa7ff58
Art Pelling <account@artpelling.de>
2026-06-10 15:59:38 +0200

    wip

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,4 @@
-import os
-
-from acoular_sphinx import build_html_context, configure_theme_options, shared_static_asset
+from acoular_sphinx import build_html_context, configure_theme_options, resolve_docs_build_config
 
 project = 'Acoular Organization'
 copyright = '2026, Acoular Organization'
@@ -12,9 +10,10 @@ extensions = [
     'sphinx_design',
 ]
 
+docs_build = resolve_docs_build_config()
+
 html_theme = 'pydata_sphinx_theme'
 html_context = build_html_context()
 html_theme_options = configure_theme_options(use_edit_page_button=False)
-html_favicon = shared_static_asset('acoular_logo.ico')
-html_baseurl = os.environ.get('DOCS_BASEURL', '')
+html_baseurl = docs_build['html_baseurl']
 exclude_patterns = ['_build']
